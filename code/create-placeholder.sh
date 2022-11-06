0bcbb76af7224a767f5cddac8d4d2282249de22c
void-echo <beihai0616@gmail.com>
2022-11-06 19:44:56 +0800

    little update

---
+++ code/create-placeholder.sh
@@ -0,0 +1,16 @@
+declare -a arr=("bin" "demo0" "demo1", "filesys", "lab2", "lab3", "lab4", "lab5", "lab6", "lab7", "monitor", "network", "test", "threads", "userprog")
+
+declare -a brr = ("dec-alpha-osf", "dec-mips-ultrix", "sun-sparc-sunos", "unknown-i386-linux")
+
+declare -a crr = ("bin", "depends", "objects")
+
+for i in "${arr[@]}"
+do
+    for j in "${brr[@]}"
+    do
+        for k in "${crr[@]}"
+        do
+            mkdir -p $i/$j/$k
+        done
+    done
+done
